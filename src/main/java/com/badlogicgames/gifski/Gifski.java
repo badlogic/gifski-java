e54218e6046df4a364134721d391670e3cd553d8
Nathan Sweet <nathan.sweet@gmail.com>
2018-05-05 11:06:49 +0200

    Moved main to test class

--- src/main/java/com/badlogicgames/gifski/Gifski.java
+++ src/main/java/com/badlogicgames/gifski/Gifski.java
@@ -95,29 +95,4 @@ public class Gifski {
 	private static native int _write (long handle, byte[] fileName);
 
 	private static native void _drop (long handle);
-
-	public static void main (String[] args) {
-		System.out.println("Starting Gifski");
-		GifskiSettings settings = new GifskiSettings();
-		Gifski gifski = new Gifski(settings);
-
-		byte[] frame = new byte[256 * 256 * 4];
-
-		gifski.start("output.gif");
-
-		for (int j = 0; j < 50; j++) {
-			for (int i = 0; i < 256*256*4; i+=4) {
-				byte val = (byte)(j * (256 / 60f));
-				frame[i] = val;
-				frame[i + 1] = (byte)0x0;
-				frame[i + 2] = val;
-				frame[i + 3] = (byte)0xff;
-			}
-			gifski.addFrameRGBA(j, 256, 256, frame, (short)4);
-		}
-		gifski.end();
-
-		gifski.dispose();
-		System.out.println("Great success!");
-	}
 }
--- src/test/java/com/badlogicgames/pngquant/GifskiTest.java
+++ src/test/java/com/badlogicgames/pngquant/GifskiTest.java
@@ -13,4 +13,29 @@ public class GifskiTest {
 		Gifski gifski = new Gifski(settings);
 		gifski.dispose();
 	}
+	
+	public static void main (String[] args) {
+		System.out.println("Starting Gifski");
+		GifskiSettings settings = new GifskiSettings();
+		Gifski gifski = new Gifski(settings);
+
+		byte[] frame = new byte[256 * 256 * 4];
+
+		gifski.start("output.gif");
+
+		for (int j = 0; j < 50; j++) {
+			for (int i = 0; i < 256 * 256 * 4; i += 4) {
+				byte val = (byte)(j * (256 / 60f));
+				frame[i] = val;
+				frame[i + 1] = (byte)0x0;
+				frame[i + 2] = val;
+				frame[i + 3] = (byte)0xff;
+			}
+			gifski.addFrameRGBA(j, 256, 256, frame, (short)4);
+		}
+		gifski.end();
+
+		gifski.dispose();
+		System.out.println("Great success!");
+	}
 }
