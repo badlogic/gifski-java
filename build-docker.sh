d619e16f19a5c00b030a8dbffda02ce2a0a9b664
badlogic <badlogicgames@gmail.com>
2018-05-07 10:18:06 +0200

    Fixed up docker invoke script.

--- build-docker.sh
+++ build-docker.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t pngquant-docker . && docker run --rm -v "$(pwd):/code" -v "$(pwd)/cargo-config:/root/.cargo/config" -it pngquant-docker
+docker build -t pngquant-docker . && docker run --rm -v "$(pwd):/code" -it pngquant-docker
